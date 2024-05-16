9e52f2fa75df37b30344a2d4a0e14ca2f32aee6d
mytype4 <ddori0430@gmail.com>
2024-05-16 21:27:13 +0900

    code edit

--- Test01/src/test01/Simulation.java
+++ Test01/src/test01/Simulation.java
@@ -15,25 +15,59 @@ public class Simulation {
         List<Menu> menuList = new ArrayList<>();
 
         // Customer 객체 리스트 생성
-        customers.add(new Customer("Customer1"));
-        customers.add(new Customer("Customer2"));
-        customers.add(new Customer("Customer3"));
+        customers.add(new Customer("아름이"));
+        customers.add(new Customer("민석이"));
+        customers.add(new Customer("주영이"));
+        customers.add(new Customer("하랑이"));
 
         // Owner 객체 리스트 생성
-        owners.add(new Owner("Owner1"));
-        owners.add(new Owner("Owner2"));
-        owners.add(new Owner("Owner3"));
+        owners.add(new Owner("도식사장"));
+        owners.add(new Owner("광운사장"));
+        owners.add(new Owner("민식이 삼촌"));
 
         customers.get(0).introduce();
 
-        owners.get(0).createRestaurant("광운이네");
-        owners.get(0).createRestaurant("광운광운이");
-        owners.get(0).addMenuToRestaurant("광운이네", "맛있는 음식1", 10000, menuList);
-        owners.get(0).addMenuToRestaurant("광운광운이", "떡볶이", 110000, menuList);
+        // 도전하라! 식당 메뉴 추가
+        owners.get(0).createRestaurant("도전하라! 식당");
+        owners.get(0).addMenuToRestaurant("도전하라! 식당", "붕어빵 맛 초코파이", 10000, menuList);
+        owners.get(0).addMenuToRestaurant("도전하라! 식당", "치즈 맛 사과", 11000, menuList);
+        owners.get(0).addMenuToRestaurant("도전하라! 식당", "핫도그 맛 아이스크림", 12000, menuList);
+        owners.get(0).addMenuToRestaurant("도전하라! 식당", "닭가슴살 맛 솜사탕", 13000, menuList);
 
-        owners.get(1).createRestaurant("광운이네");
-        owners.get(1).createRestaurant("광운광운이");
+        // 마법의 빵집 빵팡 메뉴 추가
+        owners.get(0).createRestaurant("마법의 빵집 빵팡");
+        owners.get(0).addMenuToRestaurant("마법의 빵집 빵팡", "꿈을 이루는 빵", 15000, menuList);
+        owners.get(0).addMenuToRestaurant("마법의 빵집 빵팡", "사랑을 찾는 빵", 16000, menuList);
+        owners.get(0).addMenuToRestaurant("마법의 빵집 빵팡", "복권에 당첨되는 빵", 17000, menuList);
+        owners.get(0).addMenuToRestaurant("마법의 빵집 빵팡", "건강을 되찾는 빵", 18000, menuList);
 
+        // 미래 식당 2042 메뉴 추가
+        owners.get(1).createRestaurant("미래 식당 2042");
+        owners.get(1).addMenuToRestaurant("미래 식당 2042", "곤충 단백질 샌드위치", 20000, menuList);
+        owners.get(1).addMenuToRestaurant("미래 식당 2042", "3D 프린팅 피자", 21000, menuList);
+        owners.get(1).addMenuToRestaurant("미래 식당 2042", "해조류 샐러드", 22000, menuList);
+        owners.get(1).addMenuToRestaurant("미래 식당 2042", "로봇이 만든 칵테일", 23000, menuList);
+        owners.get(1).addMenuToRestaurant("미래 식당 2042", "드론배달 컵라면", 24000, menuList);
+
+        // 시간 여행자의 숨겨진 맛 메뉴 추가
+        owners.get(2).createRestaurant("시간 여행자의 숨겨진 맛");
+        owners.get(2).addMenuToRestaurant("시간 여행자의 숨겨진 맛", "공룡 뼈 화석 스테이크", 25000, menuList);
+        owners.get(2).addMenuToRestaurant("시간 여행자의 숨겨진 맛", "미래형 영양 캡슐", 26000, menuList);
+        owners.get(2).addMenuToRestaurant("시간 여행자의 숨겨진 맛", "중세 시대 왕족 만찬", 27000, menuList);
+
+        // 악취의 향연 식당 메뉴 추가
+        owners.get(2).createRestaurant("악취의 향연 식당");
+        owners.get(2).addMenuToRestaurant("악취의 향연 식당", "취두부 냄새 샌드위치", 28000, menuList);
+        owners.get(2).addMenuToRestaurant("악취의 향연 식당", "썩은 생선 꼬치구이", 29000, menuList);
+        owners.get(2).addMenuToRestaurant("악취의 향연 식당", "땀 냄새 치즈 샐러드", 30000, menuList);
+
+        // 불안정한 맛 전문점 메뉴 추가
+        owners.get(2).createRestaurant("불안정한 맛 전문점");
+        owners.get(2).addMenuToRestaurant("불안정한 맛 전문점", "딱딱한 샌드위치", 31000, menuList);
+        owners.get(2).addMenuToRestaurant("불안정한 맛 전문점", "끈적끈적한 롤빵", 32000, menuList);
+        owners.get(2).addMenuToRestaurant("불안정한 맛 전문점", "거친 아이스크림", 33000, menuList);
+        owners.get(2).addMenuToRestaurant("불안정한 맛 전문점", "미끄러운 샐러드", 34000, menuList);
+        
         // 비동기적으로 고객들이 행동하게 하기 위한 스케줄러 설정
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(customers.size());
         for (Customer customer : customers) {
